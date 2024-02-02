cabdcaa5b78e8d44898bf937f1211df71d1235f0
ericasoares <soares.desenvolvimento@gmail.com>
2024-02-02 16:31:40 -0300

    ajuste Endereco

--- src/main/java/io/github/ericasoares/api/clinica/domain/Endereco.java
+++ src/main/java/io/github/ericasoares/api/clinica/domain/Endereco.java
@@ -15,7 +15,7 @@ public class Endereco {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+    private Long id;
     private String logradouro;
     private String bairro;
     private String cep;
