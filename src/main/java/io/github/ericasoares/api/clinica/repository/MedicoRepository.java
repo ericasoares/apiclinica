09af85adc5a35890a9a5a4dc9652868c2b2b3789
ericasoares <soares.desenvolvimento@gmail.com>
2024-02-06 17:42:59 -0300

    ajustes

--- src/main/java/io/github/ericasoares/api/clinica/repository/MedicoRepository.java
+++ src/main/java/io/github/ericasoares/api/clinica/repository/MedicoRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
-
     Page<Medico> findAllByAtivoTrue(Pageable paginacao);
 }
